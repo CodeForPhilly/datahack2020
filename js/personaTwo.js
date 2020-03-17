d8312ef1e9990b88c71300d0ef0fa57da883abca
PeterArriaza <peter.arriaza@gmail.com>
2020-03-17 17:14:20 -0400

    finished michael??

--- js/personaTwo.js
+++ js/personaTwo.js
@@ -43,7 +43,24 @@ function handleStepEnter(response) {
   //   return i === response.index;
   // });
 
+  let crisisCircle = document.getElementById('Ellipse 14');
+  let crisisText = document.getElementById('Crisis');
+  let crisisTimeline = [crisisCircle, crisisText];
+
+  let usingCircle = document.getElementById('Ellipse 16');
+  let usingText = document.getElementById('Using');
+  let usingTimeline = [usingCircle, usingText];
+
+  let treatmentCircle = document.getElementById('Ellipse 17');
+  let treatmentText = document.getElementById('Treatment');
+  let treatmentTimeline = [treatmentCircle, treatmentText];
+
+  let recoveryCircle = document.getElementById('Ellipse 15');
+  let recoveryText = document.getElementById('Recovery');
+  let recoveryTimeline = [recoveryCircle, recoveryText];
+
   console.log(response.index);
+  // timeline - crisis
   if (response.index === 0) {
     document.getElementById('image').className = 'refusal';
     figure
@@ -51,26 +68,54 @@ function handleStepEnter(response) {
       .text(
         'The blue line and shaded area show the trend of people who have overdosed increasingly refusing transport to hospitals after treatment by emergency medical personnel.'
       );
-  } else if (response.index === 1) {
+    // progress bar updates
+    crisisTimeline.map(node => node.classList.add('timeline-highlight'));
+
+    usingTimeline.map(node => node.classList.remove('timeline-highlight'));
+  }
+  // timeline - using
+  else if (response.index === 1) {
     document.getElementById('image').className = 'homeless';
     figure.select('h4').text('');
-  } else if (response.index === 2) {
+
+    // progress bar updates
+    crisisTimeline.map(node => node.classList.remove('timeline-highlight'));
+    usingTimeline.map(node => node.classList.add('timeline-highlight'));
+  }
+  // timeline - crisis
+  else if (response.index === 2) {
     document.getElementById('image').className = 'homeless-pie-charts';
     figure.select('p').text('');
     figure.select('h4').text('');
-  } else if (response.index === 3) {
+    // progress bar updates
+    crisisTimeline.map(node => node.classList.add('timeline-highlight'));
+    usingTimeline.map(node => node.classList.remove('timeline-highlight'));
+  }
+  // timeline - using
+  else if (response.index === 3) {
     document.getElementById('image').className = 'narcan';
     figure
       .select('h4')
       .text(
         'The number of Naloxone, or Narcan, administrations by emergency personnel went down as the number of people refusing transport to a hospital after these administrations went up.'
       );
-  } else if (response.index === 4) {
+    // progress bar updates
+    crisisTimeline.map(node => node.classList.remove('timeline-highlight'));
+    usingTimeline.map(node => node.classList.add('timeline-highlight'));
+  }
+  // timeline - crisis
+  else if (response.index === 4) {
     figure.select('p').text('');
     document.getElementById('image').className = 'syringe';
     figure.select('h4').text('');
     document.getElementById('caption').classList.remove('insurance-text');
-  } else if (response.index === 5) {
+    // progress bar updates
+    crisisTimeline.map(node => node.classList.add('timeline-highlight'));
+    usingTimeline.map(node => node.classList.remove('timeline-highlight'));
+    treatmentTimeline.map(node => node.classList.remove('timeline-highlight'));
+  }
+  // timeline - treatment
+  else if (response.index === 5) {
     document.getElementById('image').className = 'insurance';
     figure
       .select('h4')
@@ -78,21 +123,39 @@ function handleStepEnter(response) {
         'As of 2017, many people with no insurance were still being treated at publicly funded facilities in Phila in every category, but Medicaid was paying for most treatments. Residential treatment, however, comprised only 20% of total treatments, and almost all of that was short-term, meaning 30 days or less. Long-term residential treatment accounted for only 3% of the total.'
       );
     document.getElementById('caption').classList.add('insurance-text');
-  } else if (response.index === 6) {
+    // progress bar updates
+    crisisTimeline.map(node => node.classList.remove('timeline-highlight'));
+    treatmentTimeline.map(node => node.classList.add('timeline-highlight'));
+    usingTimeline.map(node => node.classList.remove('timeline-highlight'));
+  }
+  // timeline - using
+  else if (response.index === 6) {
     figure.select('h4').text('');
     document.getElementById('image').className = 'homeless-icon';
     document.getElementById('caption').classList.remove('insurance-text');
-  } else if (response.index === 7) {
+    // progress bar updates
+    usingTimeline.map(node => node.classList.add('timeline-highlight'));
+    treatmentTimeline.map(node => node.classList.remove('timeline-highlight'));
+  }
+  // timeline - treatment
+  else if (response.index === 7) {
     document.getElementById('image').className = 'treatments';
     figure
       .select('h4')
       .text(
         'More than half the recorded treatments for homeless people in Phila in 2017 were for 24-hour detox programs. This was double the rate for people who were not homeless, who also had much higher rates of residential treatment and medically assisted treatment (MAT).'
       );
-  } else if (response.index === 8) {
+    usingTimeline.map(node => node.classList.remove('timeline-highlight'));
+    treatmentTimeline.map(node => node.classList.add('timeline-highlight'));
+    recoveryTimeline.map(node => node.classList.remove('timeline-highlight'));
+  }
+  // timeline - recovery
+  else if (response.index === 8) {
     figure.select('h4').text('');
     document.getElementById('image').className = '';
     document.getElementById('image').className = 'support-group';
+    recoveryTimeline.map(node => node.classList.add('timeline-highlight'));
+    treatmentTimeline.map(node => node.classList.remove('timeline-highlight'));
   }
   //   figure.select('p').text('');
   //   document.getElementById('image').className = 'mat1';
